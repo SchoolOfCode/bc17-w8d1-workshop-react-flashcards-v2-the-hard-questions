481326ca277a8866db8969b8ca942d2d1955f920
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-08-19 11:08:54 +0100

    header component made

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Header.module.css';
+
+export default function Header () {
+
+  return (
+    <>
+    <div className = "header">
+      <img src= ""></img>    
+      <h1>React Flashcards</h1>
+      <p>🧠 Expand Yout React Knowledge, One Flashcard at a Time! 🐌</p>
+    </div>   
+    </>
+  )
+}
